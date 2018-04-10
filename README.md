65c0741ce434f790dc35ca6f9ee969814060dbe8
jacky123200 <jacky123200@gmail.com>
2018-04-10 12:50:19 +0800

    update

--- README.md
+++ README.md
@@ -9,8 +9,14 @@ RecyclerView 提供下列內建的版面配置管理員(layout manager)：
 + [GridLayoutManager](https://developer.android.com/reference/android/support/v7/widget/GridLayoutManager.html) 會在網格中顯示項目。
 + [StaggeredGridLayoutManager](https://developer.android.com/reference/android/support/v7/widget/StaggeredGridLayoutManager.html) 會在交錯網格中顯示項目。
 
-## Integration
-1. Add to gradle
+## 程式碼
+### 1. 加入Gradle
+ 一開始需要在Gradle加入這2行
+```gradle
+  compile 'com.android.support:cardview-v7:21.0.+'
+  compile 'com.android.support:recyclerview-v7:21.0.+'
+```
+後面的版本請根據appcompat版本作修改才可以正常運作
 ```gradle
 def LibVersion="{26.1.0}"
 
@@ -23,7 +29,7 @@ dependencies {
 }
 ```
 
-2. Add recycle view to layout
+### 2. Add recycle view to layout
 ```xml
 <?xml version="1.0" encoding="utf-8"?>
 <LinearLayout xmlns:android="http://schemas.android.com/apk/res/android"
@@ -37,7 +43,7 @@ dependencies {
 </LinearLayout>
 ```
 
-3. Add RecyclerView to activity
+### 3. Add RecyclerView to activity
 + RecyclerView (recyclerView)
 + RecyclerView.Adapter (adapter for data)
 + RecyclerView.LayoutManager (layout manager for decide display pattern)
@@ -66,5 +72,58 @@ public class MyActivity extends Activity {
         mRecyclerView.setAdapter(mAdapter);
     }
     ...
+}
+```
+
+### 4. Create adapter
+
+```java
+public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
+    private String[] mDataset;
+
+    // Provide a reference to the views for each data item
+    // Complex data items may need more than one view per item, and
+    // you provide access to all the views for a data item in a view holder
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        // each data item is just a string in this case
+        public TextView mTextView;
+        public ViewHolder(TextView v) {
+            super(v);
+            mTextView = v;
+        }
+    }
+
+    // Provide a suitable constructor (depends on the kind of dataset)
+    public MyAdapter(String[] myDataset) {
+        mDataset = myDataset;
+    }
+
+    // Create new views (invoked by the layout manager)
+    @Override
+    public MyAdapter.ViewHolder onCreateViewHolder(ViewGroup parent,
+                                                   int viewType) {
+        // create a new view
+        View v = LayoutInflater.from(parent.getContext())
+                               .inflate(R.layout.my_text_view, parent, false);
+        // set the view's size, margins, paddings and layout parameters
+        ...
+        ViewHolder vh = new ViewHolder(v);
+        return vh;
+    }
+
+    // Replace the contents of a view (invoked by the layout manager)
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        // - get element from your dataset at this position
+        // - replace the contents of the view with that element
+        holder.mTextView.setText(mDataset[position]);
+
+    }
+
+    // Return the size of your dataset (invoked by the layout manager)
+    @Override
+    public int getItemCount() {
+        return mDataset.length;
+    }
 }
 ```
--- app/src/main/java/com/example/jacky/android_java_recycleviewcardview_demo/RecycleViewActivity.java
+++ app/src/main/java/com/example/jacky/android_java_recycleviewcardview_demo/RecycleViewActivity.java
@@ -7,6 +7,10 @@ import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.StaggeredGridLayoutManager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
 /**
  * Created by Jacky on 2018/4/10.
@@ -16,7 +20,7 @@ public class RecycleViewActivity extends AppCompatActivity {
     private RecyclerView mRecyclerView;
     private RecyclerView.Adapter mAdapter;
     private RecyclerView.LayoutManager mLayoutManager;
-
+    private int defaultSpanCount = 3;
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
@@ -33,12 +37,62 @@ public class RecycleViewActivity extends AppCompatActivity {
                 mLayoutManager = new LinearLayoutManager(this);
                 break;
             case "gridlayout":
-                mLayoutManager = new GridLayoutManager(this,);
+                mLayoutManager = new GridLayoutManager(this,defaultSpanCount);
                 break;
             case "staggeredgridlayout":
-                mLayoutManager = new StaggeredGridLayoutManager();
+                mLayoutManager = new StaggeredGridLayoutManager(defaultSpanCount,StaggeredGridLayoutManager.VERTICAL);
                 break;
         }
         mRecyclerView.setLayoutManager(mLayoutManager);
+
+        mAdapter = new MyAdapter(myDataset);
+        mRecyclerView.setAdapter(mAdapter);
+    }
+
+    public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
+        private String[] mDataset;
+
+        // Provide a reference to the views for each data item
+        // Complex data items may need more than one view per item, and
+        // you provide access to all the views for a data item in a view holder
+        public class ViewHolder extends RecyclerView.ViewHolder {
+            // each data item is just a string in this case
+            public TextView mTextView;
+            public ViewHolder(TextView v) {
+                super(v);
+                mTextView = v;
+            }
+        }
+
+        // Provide a suitable constructor (depends on the kind of dataset)
+        public MyAdapter(String[] myDataset) {
+            mDataset = myDataset;
+        }
+
+        // Create new views (invoked by the layout manager)
+        @Override
+        public MyAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            // create a new view
+            View v = LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.normal_item, parent, false);
+            // set the view's size, margins, paddings and layout parameters
+
+            ViewHolder vh = new ViewHolder(v);
+            return vh;
+        }
+
+        // Replace the contents of a view (invoked by the layout manager)
+        @Override
+        public void onBindViewHolder(ViewHolder holder, int position) {
+            // - get element from your dataset at this position
+            // - replace the contents of the view with that element
+            holder.mTextView.setText(mDataset[position]);
+        }
+
+        // Return the size of your dataset (invoked by the layout manager)
+        @Override
+        public int getItemCount() {
+            return mDataset.length;
+        }
     }
 }
